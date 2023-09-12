3662e4925d618b3ee38d046758e35cd224f928c0
Mbali2000 <107546127+Mbali2000@users.noreply.github.com>
2023-09-12 19:42:32 +0200

    Changes and shop grid added

---
+++ app/src/main/java/com/example/foodiesg1/OverviewGrid.kt
@@ -0,0 +1,11 @@
+package com.example.foodiesg1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OverviewGrid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview_grid)
+    }
+}
---
+++ app/src/main/java/com/example/foodiesg1/Shop.kt
@@ -0,0 +1,3 @@
+package com.example.foodiesg1
+
+data class Shop()

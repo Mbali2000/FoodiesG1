878d2f7257d11001fd2afd2d38693543dfa2b66b
Mbali2000 <107546127+Mbali2000@users.noreply.github.com>
2023-09-14 00:23:38 +0200

    Changed grid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.android.support.cardview-v7:28.0.0")
+    implementation("com.android.support:gridlayout-v7:28.0.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 

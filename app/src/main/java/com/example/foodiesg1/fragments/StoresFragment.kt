3f3c3e96e794bbfc3079ad4839a1cd39143a0a23
sisipho21 <tkssis001@myuct.ac.za>
2023-09-18 19:50:04 +0200

    Add Store details

--- app/src/main/java/com/example/foodiesg1/fragments/StoresFragment.kt
+++ app/src/main/java/com/example/foodiesg1/fragments/StoresFragment.kt
@@ -9,14 +9,19 @@ import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.foodiesg1.R
 import com.example.foodiesg1.databinding.FragmentHomeBinding
 import com.example.foodiesg1.databinding.FragmentStoresBinding
+import com.example.foodiesg1.utils.adapter.StoreAdapter
 import com.example.foodiesg1.utils.model.StoreData
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class StoresFragment : Fragment(), StorePopUpFragment.PopUpNextListeners {
@@ -26,6 +31,9 @@ class StoresFragment : Fragment(), StorePopUpFragment.PopUpNextListeners {
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var popUpFragment: StorePopUpFragment
+    private lateinit var adapter: StoreAdapter
+    private lateinit var list: MutableList<StoreData>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,8 +47,39 @@ class StoresFragment : Fragment(), StorePopUpFragment.PopUpNextListeners {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init(view)
+        getDataFromFirebase("afr001")
         registerEvents()
 
+    }
+
+    private fun getDataFromFirebase(shopId: String){
+        /*databaseRef.child(shopId).get().addOnSuccessListener {
+            if (it.exists()){
+                val name = it.child("storeName").value
+                val cuisine = it.child("storeCuisine").value
+                val time = it.child("storeTime").value
+                Toast.makeText(context, "Store successfully found", Toast.LENGTH_SHORT).show()
+                binding.storename.text = name.toString()
+                binding.storecuisine.text = cuisine.toString()
+                binding.storetime.text = time.toString()
+
+            }else {
+                Toast.makeText(context, "Store not found", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show()
+        }
+        databaseRef.addValueEventListener(object : ValueEventListener){
+            override fun onDataChange(snapshot: DataSnapshot){
+                list.clear()
+                for (shopSnap in snapshot.children){
+                    val
+                }
+            }
+            override fun onCancelled(error: DatabaseError){
+                Toast.makeText(context, "Failed to save your store", Toast.LENGTH_SHORT).show()
+            }
+        }*/
     }
 
     private fun registerEvents(){
@@ -59,6 +98,13 @@ class StoresFragment : Fragment(), StorePopUpFragment.PopUpNextListeners {
         navController = Navigation.findNavController(view)
         auth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().reference.child("Stores")
+
+        binding.storeRecyclerView.setHasFixedSize(true)
+        binding.storeRecyclerView.layoutManager = GridLayoutManager(context, 2)
+
+        adapter = StoreAdapter(list)
+        adapter.setListener(this)
+        binding.storeRecyclerView.adapter = adapter
     }
 
     override fun onSaveTask(
---
+++ app/src/main/java/com/example/foodiesg1/utils/adapter/StoreAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.foodiesg1.utils.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodiesg1.databinding.EachStoreBinding
+import com.example.foodiesg1.utils.model.StoreData
+
+class StoreAdapter(private val list: MutableList<StoreData>) :
+    RecyclerView.Adapter<StoreAdapter.StoreViewHolder>() {
+
+    private var listener:StoreAdapterClicksInterface? = null
+    fun setListener(listener:StoreAdapterClicksInterface){
+        this.listener = listener
+    }
+
+    inner class StoreViewHolder(binding: EachStoreBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreViewHolder {
+        val binding = EachStoreBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StoreViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: StoreViewHolder, position: Int) {
+        /*with(holder){
+            with(list[position]){
+
+                binding.storename.text = this.storeName
+                binding.storecuisine.text = this.storeCuisine
+
+                binding.storetime.text = this.storeTime
+
+                binding.deletestore.setOnClickListener{
+                    listener?.onDeleteStoreClicked(this)
+                }
+
+                binding.editstore.setOnClickListener{
+                    listener?.onUpdateStoreClicked(this)
+                }
+            }
+        }*/
+    }
+
+    interface StoreAdapterClicksInterface{
+        fun onDeleteStoreClicked(storeData: StoreData)
+        fun onUpdateStoreClicked(storeData: StoreData)
+    }
+}

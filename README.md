766e1006e14839dfc0e670378248f4eb0aa53b8c
MatomeGafane <matomegafanework@gmail.com>
2023-09-25 21:50:48 +0200

    Added ReadMe
    Added ReadMe

---
+++ README.md
@@ -0,0 +1,63 @@
+# Foodies App
+
+Foodies is a mobile application that allows users to explore restaurants, view menus, add items to favorites, and more. This README provides instructions for setting up and running the app.
+
+## Getting Started
+
+To get started with Foodies, follow these steps:
+
+### Prerequisites
+
+- [Android Studio](https://developer.android.com/studio) installed on your development machine.
+- Android emulator or a physical Android device connected to your machine.
+
+### Installation
+
+1. Clone the repository to your local machine using Git:
+
+   ```bash
+   git clone https://gitlab.com/your-gitlab-username/foodies-app.git
+
+Open Android Studio.
+
+Click on "File" > "Open" and navigate to the foodies-app directory that you just cloned.
+
+Android Studio will automatically recognize the project and build the necessary files.
+
+Running the App
+Make sure you have an Android emulator running or a physical Android device connected to your machine.
+
+In Android Studio, click the green "Run" icon (or press Shift + F10) to build and install the app on the emulator or device.
+
+Wait for the app to build and install.
+
+Once installed, you can open the Foodies app on your emulator or device.
+
+Usage
+Upon opening the app, you will have options to log in, register, or continue without logging in.
+
+Explore restaurants, view their menus, and add your favorite items.
+
+You can also search for specific menu items or restaurants.
+
+Users can view, comment on, and favorite menu items. Menu items can be downloaded in PDF format for later access.
+
+All users can edit their profiles via the Profile page.
+
+Contributors
+Matome Gafane
+Mbali Myeza
+Sisipho Tukushe
+
+If you encounter any issues or have questions, please feel free to contact the contributors listed above or open an issue in the GitLab repository.
+
+Happy reviewing with Foodies!
+
+
+
+
+
+
+
+
+ 
---
+++ app/src/main/java/com/foodsoncampus/foodies/activities/DashboardAdminActivity.java
@@ -0,0 +1,155 @@
+package com.foodsoncampus.foodies.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.foodsoncampus.foodies.adapters.AdapterRestaurant;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.foodsoncampus.foodies.databinding.ActivityDashboardAdminBinding;
+import com.foodsoncampus.foodies.models.ModelCategory;
+
+import java.util.ArrayList;
+
+public class DashboardAdminActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityDashboardAdminBinding binding;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    //arraylist to store category
+    private ArrayList<ModelCategory> categoryArrayList;
+    //adapter
+    private AdapterRestaurant adapterRestaurant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+        loadCategories();
+
+
+        //edit text change listern, search
+//        binding.searchEt.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                //called as and when user type each letter
+//                try {
+//                    adapterRestaurant.getFilter().filter(s);
+//                }
+//                catch (Exception e){
+//
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+
+        //handle click, logout
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+
+        //handle click, start restaurant add screen
+        binding.addRestaurantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardAdminActivity.this, RestaurantAddActivity.class));
+            }
+        });
+
+        //handle click, start pdf add screen
+        binding.addPdfFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardAdminActivity.this, PdfAddActivity.class));
+            }
+        });
+
+        //handle click, open profile
+        binding.profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardAdminActivity.this, ProfileActivity.class));
+            }
+        });
+
+
+    }
+
+    private void loadCategories() {
+        //init araylist
+        categoryArrayList = new ArrayList<>();
+
+        //get all categories from firebase > Categories
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Restaurants");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //clear arraylist before adding data into it
+                categoryArrayList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    //get data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+
+                    //add to arraylist
+                    categoryArrayList.add(model);
+                }
+                //setup adapter
+                adapterRestaurant = new AdapterRestaurant(DashboardAdminActivity.this, categoryArrayList);
+                //set adapter to recyclerview
+                binding.categoriesRv.setAdapter(adapterRestaurant);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void checkUser() {
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser==null){
+            //not logged in, goto main screen
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        else {
+            //logged in, get user info
+            String email = firebaseUser.getEmail();
+            //set in textview of toolbar
+            binding.subTitleTv.setText(email);
+        }
+    }
+}
